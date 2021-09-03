6ab879e7c17500ded068688a90dd25e6b1605056
yizhishang <285206405@qq.com>
2021-09-03 21:52:19 +0800

    [dev]: 加密
    Signed-off-by: yizhishang <285206405@qq.com>

--- yizhishang-common/src/main/java/com/yizhishang/common/util/RSAUtil.java
+++ yizhishang-common/src/main/java/com/yizhishang/common/util/RSAUtil.java
@@ -25,8 +25,17 @@ public class RSAUtil {
      */
     public static final String PRIVATE_KEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCxLNznOHjkccOKPSjga8j9Xbr1nY4TLPlPykW9UHTqG85LKJIo+7mb0ZheXu2cLYpcBbJ8MyozEjHZYoFKgnKppqylCLBx687oxQW6KcDAKqTJLyOjoaLjdIZpSoNVbQFha6mJTK9XAgAI/gs1EvNYeCaDYsuIixlZ7HvbJMiy5Tipk5eK71o5D9sYsWQRXnFlo+d+51FU+b6755qZLJsiGSigD82LR8wBWatJVocqRpGWCu71e/FhAESraAFtb/qM3grDvNyFjGjDqzB4fb1z4xtdrq6cb6d8APbUt5Z/OMK5VVpKuI4meN1GKSXGRRp0w4dzMUxAguai7HPSZpPhAgMBAAECggEAZJTYb3dSwjWHUbI4w3bNKnEMkyU3o9EzU8W746aKSOEyMt68YwHMtBW/z0tKzw/XbThaxEFzonygyg0u4vK9T2xstGADcxjwZJp9DttnLnDHDeNx8xHu2/up1ppThqqUTw8EtwpOvDFJfdFi4WV5um6zRoFOlJyK7s7e3WC7gzrGHTN8EAOR+SbMVo11okWdeAabD/2XF8K3EWaYU/SaIu7unJq5fMigUM9G9KX+L+LsjhyFQfpbZSGdk2IXfpl0bvboS/hidqBrpnr6UYCNPUNtwDyel9zhkLUI4bQVtnnrIMU6IpmcZS7/KkfBO+WSY9OvgFxRnXExmOptQlNGVQKBgQDZVWs5LQLcElU2bLCnszJ/7Jr4SX9YTlr7a8BPXZ2MwmX3qemx4hnyD7Hzr1ruPa7NQ5A/W7HAGCmt++zOndvYOiPoWHzQ43WUTRSQRV8Mzhr1mjbg8VYwWnZ6OA9WN52kacicNOSIpAkHXP6J1u4Ar7NVVcD6FCBVlGTwU4g9lwKBgQDQsmgeDHUsSRYiQfwm5CxmL/QVwfLjeQkNQNpZCeMHW84SHjvrZv+zf3pf1mPAytd+Z/swa2i1sDVW1OpBX2wKGZDjoCpqhrDOdPza6sat9yD+A/dMf62q+UjAHwjnG4z8654W87G/kVE7+ob5L5Snf2bNVOf1IgYb7lyI9exZRwKBgQC3/86ImKgMI+VK75hcUqWVkn7daHRNEgGLTP2C9BIYAtmLZao59N/chRc25aZJf6WHmQT97ya9KHfZGimyAAoJtaeC5a++tmRsixm2z1SapcBDYS3HIevDgkM+VEn/x5RKh1jVjx5Q5PfT6RKDJplXsWlV/QUldHavnbQbl80HvwKBgDdiIR9YTwAhpS4SNMhdZSr5Lt1XSw3y4OQTgd5/MTCSvG0yeNRP0SvheKhWTMKtEeMJ1S9UUcVlrcINXt1aZWGIx1qg9qCufd1MTo4KIA+qtaazhr1WZNAqt5PEqKgsN44nORz+Lv2Xouc35PncFqccuDtlfOkgzl47M3NlUAOhAoGBAIiGK0nCxxI8IKnkLIqTlRNYApilHfSwQJBrbOY3OMEFE9HhZOYFzcc37lAruV9VM5MGdbRQaEp/GNGxlMEnfQr6I3JVN4gyt/nP21rVBmQ9YqRDxifPkZbSt29k71h/50BmaF5IgnLB/XuPjsdI4BWLBBbarvs6qAsBCtlEH7FH";
 
-
     private static final String RSA = "RSA";
+
+    /**
+     * 字节数组转Base64编码
+     */
+    private static final Base64.Encoder encoder = Base64.getEncoder();
+
+    /**
+     * Base64编码转字节数组
+     */
+    private static final Base64.Decoder decoder = Base64.getDecoder();
 
     private RSAUtil() {
 
@@ -54,7 +63,7 @@ public class RSAUtil {
     public static String getPublicKey(KeyPair keyPair) {
         PublicKey publicKey = keyPair.getPublic();
         byte[] bytes = publicKey.getEncoded();
-        return byte2Base64(bytes);
+        return encoder.encodeToString(bytes);
     }
 
     /**
@@ -66,7 +75,7 @@ public class RSAUtil {
     public static String getPrivateKey(KeyPair keyPair) {
         PrivateKey privateKey = keyPair.getPrivate();
         byte[] bytes = privateKey.getEncoded();
-        return byte2Base64(bytes);
+        return encoder.encodeToString(bytes);
     }
 
     /**
@@ -77,7 +86,7 @@ public class RSAUtil {
      * @throws Exception 异常
      */
     public static PublicKey string2PublicKey(String publicKey) throws Exception {
-        byte[] keyBytes = base642Byte(publicKey);
+        byte[] keyBytes = decoder.decode(publicKey);
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance(RSA);
         return keyFactory.generatePublic(keySpec);
@@ -91,7 +100,7 @@ public class RSAUtil {
      * @throws Exception 异常
      */
     public static PrivateKey string2PrivateKey(String privateKey) throws Exception {
-        byte[] keyBytes = base642Byte(privateKey);
+        byte[] keyBytes = decoder.decode(privateKey);
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance(RSA);
         return keyFactory.generatePrivate(keySpec);
@@ -109,7 +118,7 @@ public class RSAUtil {
         // 将Base64编码后的公钥转换成PublicKey对象
         cipher.init(Cipher.ENCRYPT_MODE, string2PublicKey(PUBLIC_KEY));
         byte[] publicEncrypt = cipher.doFinal(content.getBytes());
-        return byte2Base64(publicEncrypt);
+        return encoder.encodeToString(publicEncrypt);
     }
 
     /**
@@ -125,7 +134,7 @@ public class RSAUtil {
         // 将Base64编码后的公钥转换成PublicKey对象
         cipher.init(Cipher.ENCRYPT_MODE, string2PublicKey(publicKey));
         byte[] publicEncrypt = cipher.doFinal(content.getBytes());
-        return byte2Base64(publicEncrypt);
+        return encoder.encodeToString(publicEncrypt);
     }
 
     /**
@@ -160,7 +169,7 @@ public class RSAUtil {
     public static String publicDecrypt(String content) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
         cipher.init(Cipher.DECRYPT_MODE, string2PublicKey(PUBLIC_KEY));
-        byte[] result = cipher.doFinal(base642Byte(content));
+        byte[] result = cipher.doFinal(decoder.decode(content));
         return new String(result);
     }
 
@@ -175,7 +184,7 @@ public class RSAUtil {
     public static String publicDecrypt(String content, String publicKey) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
         cipher.init(Cipher.DECRYPT_MODE, string2PublicKey(publicKey));
-        byte[] result = cipher.doFinal(base642Byte(content));
+        byte[] result = cipher.doFinal(decoder.decode(content));
         return new String(result);
     }
 
@@ -201,7 +210,7 @@ public class RSAUtil {
         Cipher cipher = Cipher.getInstance(RSA);
         cipher.init(Cipher.ENCRYPT_MODE, string2PrivateKey(PRIVATE_KEY));
         byte[] result = cipher.doFinal(content.getBytes());
-        return byte2Base64(result);
+        return encoder.encodeToString(result);
     }
 
     /**
@@ -216,7 +225,7 @@ public class RSAUtil {
         Cipher cipher = Cipher.getInstance(RSA);
         cipher.init(Cipher.ENCRYPT_MODE, string2PrivateKey(privateKey));
         byte[] result = cipher.doFinal(content.getBytes());
-        return byte2Base64(result);
+        return encoder.encodeToString(result);
     }
 
     /**
@@ -224,39 +233,39 @@ public class RSAUtil {
      *
      * @param content 密文
      * @return 明文
-     * @throws Exception
+     * @throws Exception 异常
      */
     public static String privateDecrypt(String content) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
         // 将Base64编码后的私钥转换成PrivateKey对象
         cipher.init(Cipher.DECRYPT_MODE, string2PrivateKey(PRIVATE_KEY));
-        byte[] result = cipher.doFinal(base642Byte(content));
+        byte[] result = cipher.doFinal(decoder.decode(content));
         return new String(result);
     }
 
     /**
      * 私钥解密
      *
-     * @param content
-     * @param privateKey
-     * @return
-     * @throws Exception
+     * @param content    密文
+     * @param privateKey 私钥字符串
+     * @return 明文
+     * @throws Exception 异常
      */
     public static String privateDecrypt(String content, String privateKey) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
         // 将Base64编码后的私钥转换成PrivateKey对象
         cipher.init(Cipher.DECRYPT_MODE, string2PrivateKey(privateKey));
-        byte[] result = cipher.doFinal(base642Byte(content));
+        byte[] result = cipher.doFinal(decoder.decode(content));
         return new String(result);
     }
 
     /**
      * 私钥解密
      *
-     * @param content
-     * @param privateKey
-     * @return
-     * @throws Exception
+     * @param content    密文
+     * @param privateKey 私钥字符串
+     * @return 明文字节数组
+     * @throws Exception 异常
      */
     public static byte[] privateDecrypt(byte[] content, PrivateKey privateKey) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
@@ -264,19 +273,4 @@ public class RSAUtil {
         return cipher.doFinal(content);
     }
 
-    /**
-     * 字节数组转Base64编码
-     */
-    public static String byte2Base64(byte[] bytes) {
-        Base64.Encoder encoder = Base64.getEncoder();
-        return encoder.encodeToString(bytes);
-    }
-
-    /**
-     * Base64编码转字节数组
-     */
-    public static byte[] base642Byte(String base64Key) {
-        Base64.Decoder decoder = Base64.getDecoder();
-        return decoder.decode(base64Key);
-    }
 }
--- yizhishang-common/src/test/java/RSADemo.java
+++ yizhishang-common/src/test/java/RSADemo.java
@@ -17,22 +17,14 @@ public class RSADemo {
      */
     public void client2Server() {
         try {
-            /**
-             * ===============生成公钥和私钥，公钥传给客户端，私钥服务端保留==================
-             */
-            System.out.println("客户端（公钥加密）发送给服务端（私钥解密）----------------------------------------------------");
-
             String message = "yizhishang";
-            // 加密后的内容Base64编码
-            String byte2Base64 = RSAUtil.publicEncrypt(message);
-            System.out.println("公钥加密并Base64编码的结果：" + byte2Base64);
-            // ############## 网络上传输的内容有Base64编码后的公钥 和 Base64编码后的公钥加密的内容 #################
+            System.out.println("加密+解密---------------------------------------------------- 明文：" + message);
 
-            // ===================服务端================
-            // 用私钥解密
+            String byte2Base64 = RSAUtil.publicEncrypt(message);
+            System.out.println("客户端公钥加密并Base64编码的结果：" + byte2Base64);
+
             String content = RSAUtil.privateDecrypt(byte2Base64);
-            // 解密后的明文
-            System.out.println("解密后的明文: " + content);
+            System.out.println("服务端私钥解密: " + content);
             System.out.println();
         } catch (Exception e) {
             e.printStackTrace();
@@ -44,23 +36,14 @@ public class RSADemo {
      */
     public void server2Client() {
         try {
-            // ===============生成公钥和私钥，公钥传给客户端，私钥服务端保留==================
-            System.out.println("服务端（私钥签名）发给客户端（公钥验签）----------------------------------------------------");
-
-            // =================服务端=================
             String message = "50.36";
-            // 将Base64编码后的私钥转换成PrivateKey对象
-            // 用私钥签名
-            // 加密后的内容Base64编码
+            System.out.println("签名+验签---------------------------------------------------- 明文：" + message);
+
             String byte2Base64 = RSAUtil.privateEncrypt(message);
-            System.out.println("私钥签名：" + byte2Base64);
+            System.out.println("服务端私钥签名：" + byte2Base64);
 
-            // ############## 网络上传输的内容有 Base64编码后的私钥签名的内容 #################
-
-            // ===================客户端================
-            // 用公钥验签
             String content = RSAUtil.publicDecrypt(byte2Base64);
-            System.out.println("公钥验签：" + content);
+            System.out.println("客户端公钥验签：" + content);
         } catch (Exception e) {
             e.printStackTrace();
         }
