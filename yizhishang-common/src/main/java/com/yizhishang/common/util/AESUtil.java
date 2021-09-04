da431fe8a79af42d78f4289f97bdbe25998dc7d7
yizhishang <285206405@qq.com>
2021-09-04 23:48:37 +0800

    [dev]: AES加密代码优化
    Signed-off-by: yizhishang <285206405@qq.com>

--- yizhishang-common/src/main/java/com/yizhishang/common/util/AESUtil.java
+++ yizhishang-common/src/main/java/com/yizhishang/common/util/AESUtil.java
@@ -6,6 +6,7 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.logging.Level;
@@ -16,7 +17,7 @@ import java.util.logging.Logger;
  */
 public class AESUtil {
 
-    private static final int length = 128;
+    private static final int LENGTH = 128;
 
     private static final String KEY_ALGORITHM = "AES";
     /**
@@ -34,7 +35,7 @@ public class AESUtil {
     public static String encrypt(String content, String key) {
         try {
             Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
-            byte[] byteContent = content.getBytes("UTF-8");
+            byte[] byteContent = content.getBytes(StandardCharsets.UTF_8);
             cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(key));
             byte[] result = cipher.doFinal(byteContent);
             return Base64Utils.encodeToString(result);
@@ -59,7 +60,7 @@ public class AESUtil {
 
             //执行操作
             byte[] result = cipher.doFinal(Base64Utils.decode(content.getBytes()));
-            return new String(result, "UTF-8");
+            return new String(result, StandardCharsets.UTF_8);
         } catch (Exception ex) {
             Logger.getLogger(AESUtil.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -69,16 +70,16 @@ public class AESUtil {
     /**
      * 生成加密秘钥
      *
-     * @param key
+     * @param key key值
      * @return 秘钥
      */
     private static SecretKeySpec getSecretKey(final String key) {
         //返回生成指定算法密钥生成器的 KeyGenerator 对象
-        KeyGenerator kg = null;
+        KeyGenerator kg;
         try {
             kg = KeyGenerator.getInstance(KEY_ALGORITHM);
             //AES 要求密钥长度为 128
-            kg.init(length, new SecureRandom(key.getBytes()));
+            kg.init(LENGTH, new SecureRandom(key.getBytes()));
 
             //生成一个密钥
             SecretKey secretKey = kg.generateKey();
