6de4df17a14a3d95535d447881fa043821871720
袁永君 <yuanyongjun1@sfmail.sf-express.com>
2021-09-06 10:00:12 +0800

    export demo
    Signed-off-by: 袁永君 <yuanyongjun1@sfmail.sf-express.com>

---
+++ yizhishang-common/src/test/java/AESDemo.java
@@ -0,0 +1,17 @@
+import com.yizhishang.common.util.AESUtil;
+
+/**
+ * @author: yizhishang
+ * @date: 2021/9/6 9:56
+ */
+public class AESDemo {
+    public static void main(String[] args) {
+        String content = "hello,您好";
+        String key = "今天是个好日子";
+        System.out.println("content:" + content);
+        String s1 = AESUtil.encrypt(content, key);
+        System.out.println("s1:" + s1);
+        System.out.println("s2:" + AESUtil.decrypt(s1, key));
+
+    }
+}
