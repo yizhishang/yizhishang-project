38398288fbec6f1050d02630f504881702b3945c
袁永君 <yuanyongjun1@sfmail.sf-express.com>
2021-10-18 17:58:12 +0800

    demo修改

--- yizhishang-common/src/test/java/util/DesensitizationDemo.java
+++ yizhishang-common/src/test/java/util/DesensitizationDemo.java
@@ -11,34 +11,34 @@ public class DesensitizationDemo {
 
     public static void main(String[] args) {
         String number = "13088840046";
-        System.out.println(DesensitizationUtil.hideNumber(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideNumber(number));
 
-        number = "420222199001058418";
-        System.out.println(DesensitizationUtil.hideNumber(number));
+        number = "420222196501058418";
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideNumber(number));
 
         number = "6228480128149967373";
-        System.out.println(DesensitizationUtil.hideNumber(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideNumber(number));
 
         number = "285206405@qq.com";
-        System.out.println(DesensitizationUtil.hideMailbox(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideMailbox(number));
 
         number = "袁永君";
-        System.out.println(DesensitizationUtil.hideUserName(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideUserName(number));
 
         number = "湖北省黄石市阳新县发送到发斯蒂芬";
-        System.out.println(DesensitizationUtil.hideAddress(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideAddress(number));
 
         number = "黄石市阳新县发送到发斯蒂芬";
-        System.out.println(DesensitizationUtil.hideAddress(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideAddress(number));
 
         number = "浙江省新华书店有限公司";
-        System.out.println(DesensitizationUtil.hideCompanyName(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideCompanyName(number));
 
         number = "顺丰控股集团有限公司";
-        System.out.println(DesensitizationUtil.hideCompanyName(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideCompanyName(number));
 
         number = "深圳市顺丰控股集团有限公司";
-        System.out.println(DesensitizationUtil.hideCompanyName(number));
+        System.out.println(number + "\t\t\t" + DesensitizationUtil.hideCompanyName(number));
 
     }
 
